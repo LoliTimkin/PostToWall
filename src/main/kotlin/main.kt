3d3567c7d5402d8afbd2f3ed7e280843c212c935
Evgeny R <ryderi700@gmail.com>
2022-04-07 22:00:20 +0300

    added array and func

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,30 @@
 fun main() {
+    var new_post = Post(id = 0,
+   owner_id = 0,
+from_id = 0,
+created_by = 0,
+date = 0,
+text = "something",
+reply_owner_id = 0,
+reply_post_id = 0,
+friends_only = 0
+//comments = null,
+//copyright: Copyright,
+//likes: Likes,
+//reposts: Reposts,
+//views: Views,
+//post_type: String,
+//signer_id: String,
+//can_pin: Boolean,
+//can_delete: Boolean,
+//can_edit: Boolean,
+//is_pinned: Boolean,
+//marked_as_ads: Boolean,
+//is_favorite: Boolean,
+//donut: Donat,
+//postponed_id: Boolean
+)
+    // WallService.add(new_post)
 
 }
 class Comment (
@@ -43,27 +69,45 @@ data class Post(
     val from_id: Int,
     val created_by: Int,
     val date: Int,
-    val text: Int,
+    val text: String,
     val reply_owner_id: Int,
     val reply_post_id: Int,
     val friends_only: Int,
-    val comments: Comment,
-    val copyright: Copyright,
-    val likes: Likes,
-    val reposts: Reposts,
-    val views: Views,
-    val post_type: String,
-    val signer_id: String,
-    val can_pin: Boolean,
-    val can_delete: Boolean,
-    val can_edit: Boolean,
-    val is_pinned: Boolean,
-    val marked_as_ads: Boolean,
-    val is_favorite: Boolean,
-    val donut: Donat,
-    val postponed_id: Boolean
+    //val comments: Comment,
+    //val copyright: Copyright,
+    //val likes: Likes,
+    //val reposts: Reposts,
+    //val views: Views,
+    //val post_type: String,
+    //val signer_id: String,
+    //val can_pin: Boolean,
+    //val can_delete: Boolean,
+    //val can_edit: Boolean,
+    //val is_pinned: Boolean,
+    //val marked_as_ads: Boolean,
+    //val is_favorite: Boolean,
+    //val donut: Donat,
+    //val postponed_id: Boolean
 )
 
 object WallService {
+
     private var posts = emptyArray<Post>()
+    private val nextId  = posts.last().id+1
+
+    fun add(post: Post): Post {
+        posts += post
+        return posts.last()
+    }
+    fun update(post: Post): Boolean {
+
+        for (pos in posts) {
+            if (post.id == pos.id) {
+                val text: String = "update"
+                println(text)
+                return true
+            }
+        }
+        return false
+    }
 }
